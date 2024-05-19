311f5ce47477e4b5b572b05c2ea1e38e521dfda6
Ifrahim-WSL <147778089+IfrahimA@users.noreply.github.com>
2024-05-19 17:49:54 -0400

    Added Documentation

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# tic-tac-toe
+## Tic-Tac-Toe
+
+The following is the representation of a Tic-Tac-Toe game.
+
+Procedure: 
+1. Creating Board
+2. Style sheet
+Javascript
+3. Create a Board, with the ability to reset it and change its properties
+4. Make it so that the board is interactive, being able to place X's and O's
+5. Creating a method to check for a winner
+6. Creating a method to check for a tie
+7. Creating a method to reset board. 
+
+**Languages:** 
+HTML, CSS, Javascript
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Create a Board
 const gameBoard = () => {
     const board = ["", "", "", "", "", "", "", "", ""];
 
@@ -14,10 +15,11 @@ const gameBoard = () => {
     return { board, setBoard, resetBoard };
 };
 
+
+//Playround function plays a round of Tic-Tac-Toe
 const playRound = () => {
-    let currentPlayer = "O";
-    let isWinner = false;
-    let isTie = false;
+
+    /* Variables */
     const squares = document.querySelectorAll(".square");
     const header = document.querySelector('.header');
     const body = document.querySelector('.body');
@@ -25,25 +27,44 @@ const playRound = () => {
     const modal = document.querySelector('.modal');
     const yesReset = document.getElementById('yesReset');
     const noReset = document.getElementById('noReset');
+    let currentPlayer = "O";
+    let isWinner = false;
+    let isTie = false;
 
+    /* Creates a Game Board*/
     gameB = gameBoard();
+
+    /* Begins the Handle Click*/
     handleClick(gameB);
+
+    /* It handles the clicks for each individual grid.
+    *@param game board
+    */
     function handleClick(board) {
         squares.forEach((e) => {
             e.addEventListener('click', () => {
                 if (e.textContent == "" && isWinner == false) {
+
+                    //Alternate the values of X and O
                     currentPlayer = (currentPlayer == "O") ? "X" : "O";
+                    //Display the values of current player's turn. 
                     tempPlayer = (currentPlayer == "X") ? "O" : "X";
+                    header.textContent = `Player ${tempPlayer}\'s Turn`;
+                    //Places the X or O on the grid. 
                     e.textContent = currentPlayer;
                     e.classList.add("square-x");
-                    header.textContent = `Player ${tempPlayer}\'s Turn`;
+
+                    //Add to gameboard array
                     board.setBoard(e.id - 1, currentPlayer);
 
                     //Check to see if there is a tie or winner!
                     winner = checkWinner(board);
                     isTie = checkTie(board);
 
-
+                    /*
+                    *If there is a winner declare it. 
+                    *If there is a tie declare it. 
+                    */
                     if (winner.result == true) {
                         header.textContent = `Player ${currentPlayer} is the Winner!`;
                         resetGame(board);
@@ -59,9 +80,15 @@ const playRound = () => {
         });
     }
 
+    /*
+    * The following function checks the winner.
+    * @param game board
+    * @return result -> true or false
+    */
     function checkWinner(gameBoard) {
         let result = false;
 
+        //Possible Combinations
         const winningCombinations =
             [[0, 1, 2],
             [3, 4, 5],
@@ -73,6 +100,7 @@ const playRound = () => {
             [2, 4, 6]];
 
 
+        //Checks to see if there is a winning combination. 
         for (let i = 0; i < winningCombinations.length; i++) {
             const [a, b, c] = winningCombinations[i];
             if (gameBoard.board[a] == gameBoard.board[b]
@@ -85,9 +113,14 @@ const playRound = () => {
         return { result };
     }
 
+    /*
+    * The following function checks to see if there is a tie.
+    * @return result -> true or false
+    */
     function checkTie() {
         let count = 0;
         let result = false;
+        //Checks to see if all 9 squares are filled. 
         squares.forEach((e) => {
             if (e.textContent == "X" || e.textContent == "O") {
                 count++;
@@ -100,10 +133,18 @@ const playRound = () => {
         return { result };
     }
 
+    /*
+    * Resets the board for the next round.
+    * @param game board
+    */
     function resetGame(board) {
+
+        //Manipulates displays
         modal.classList.remove('not-active');
         footer.classList.add('not-active');
         body.classList.add('not-active');
+
+        //Asks user if they want to continue
         yesReset.addEventListener('click', () => {
             board.resetBoard();
             for (let i = 0; i < squares.length; i++) {
@@ -115,6 +156,7 @@ const playRound = () => {
             header.textContent = `Player ${tempPlayer}\'s Turn`;
             isWinner = false;
         });
+        //Reloads the page, if user doesn't want to continue. 
         noReset.addEventListener('click', () => {
             location.reload();
         });
@@ -122,6 +164,10 @@ const playRound = () => {
 
 };
 
+/*
+* Initial Display
+* Asks user if they want to play or not. 
+*/
 function readyToPlay() {
     const yesBtn = document.getElementById('yesBtn');
     const header = document.querySelector('.header');
@@ -135,6 +181,9 @@ function readyToPlay() {
     });
 }
 
+/*
+* Executes the functions, without global scope.  
+*/
 const game = (function () {
     readyToPlay();
     playRound();
